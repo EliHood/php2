7041e900e05cbb73d46ee46e2dfed145049d7a49
POWER USER <poweruser@Users-iMac-2.local>
2017-04-06 22:59:45 -0400

    heroku update

--- config/database.php
+++ config/database.php
@@ -1,5 +1,15 @@
 <?php
 
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+
+    $conn = new mysqli($server, $username, $password, $db);
+
+
 return [
 
     /*
@@ -54,11 +64,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => $server,
             'port' => env('DB_PORT', '8889'),
-            'database' => env('DB_DATABASE', 'example'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'root'),
+            'database' =>$db,
+            'username' => $username,
+            'password' =>$password,
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
