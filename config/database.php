617f32d0937fd188fb7c2d2541fe30b853ec0c25
POWER USER <poweruser@Users-iMac-2.local>
2017-04-06 23:28:22 -0400

    update

--- config/database.php
+++ config/database.php
@@ -1,15 +1,5 @@
 <?php
 
-    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-    $server = $url["host"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $db = substr($url["path"], 1);
-
-    $conn = new mysqli($server, $username, $password, $db);
-
-
 return [
 
     /*
@@ -64,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => $server,
-            'port' => env('DB_PORT', '127.0.0.1'),
-            'database' =>$db,
-            'username' => $username,
-            'password' =>$password,
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '8889'),
+            'database' => env('DB_DATABASE', 'example'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', 'root'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
